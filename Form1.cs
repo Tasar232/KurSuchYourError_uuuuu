68bca9f24b3b80bf9ae6e022103df47531eae4d1
uitomsacc@outlook.com <uitomsacc@outlook.com>
2021-12-08 13:09:44 +0800

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        // собственно список, пока пустой
+        List<Particle> particles = new List<Particle>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
+
+             
+            for (var i = 0; i < 500; ++i)
+            {
+                var particle = new Particle();
+                // переношу частицы в центр изображения
+                //particle.X = picDisplay.Image.Width / 2;
+                //particle.Y = picDisplay.Image.Height / 2;
+                particle.X = Particle.rand.Next(1920);
+                particle.Y = Particle.rand.Next(1080);
+                // добавляю список
+                particles.Add(particle);
+            }
+        }
+
+        private void picDisplay_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        // добавил функцию обновления состояния системы
+        private void UpdateState()
+        {
+            foreach (var particle in particles)
+            {
+                particle.Life -= 1; // уменьшаю здоровье
+                                    // если здоровье кончилось
+                if (particle.Life < 0)
+                {
+                    // восстанавливаю здоровье
+                    particle.Life = 20 + Particle.rand.Next(100);
+                    // перемещаю частицу в центр
+                    particle.X = Particle.rand.Next(1920);
+                    particle.Y = Particle.rand.Next(1080);
+           
+                    particle.Direction = Particle.rand.Next(360);
+                    particle.Speed = 1 + Particle.rand.Next(10);
+                    particle.Radius = 2 + Particle.rand.Next(10);
+                }
+                else
+                {
+                    // а это наш старый код
+                    var directionInRadians = particle.Direction / 180 * Math.PI;
+                    particle.X += (float)(particle.Speed * Math.Cos(directionInRadians));
+                    particle.Y -= (float)(particle.Speed * Math.Sin(directionInRadians));
+                }
+            }
+
+           // // добавил генерацию частиц
+           // // генерирую не более 10 штук за тик
+           // for (var i = 0; i < 10; ++i)
+           // {
+           //     if (particles.Count < 500) // пока частиц меньше 500 генерируем новые
+           //     {
+           //         var particle = new Particle();
+           //         particle.X = Particle.rand.Next(360);
+           //         particle.Y = Particle.rand.Next(360);
+           //         particles.Add(particle);
+           //     }
+           //     else
+           //     {
+           //         break; // а если частиц уже 500 штук, то ничего не генерирую
+           //     }
+           // }
+        }
+
+        // функция рендеринга
+        private void Render(Graphics g)
+        {
+            // утащили сюда отрисовку частиц
+            foreach (var particle in particles)
+            {
+                particle.Draw(g);
+            }
+        }
+
+        int counter = 0; // добавлю счетчик чтобы считать вызовы функции
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            UpdateState(); // каждый тик обновляем систему
+
+            using (var g = Graphics.FromImage(picDisplay.Image))
+            {
+                g.Clear(Color.White);
+                Render(g); // рендерим систему
+            }
+
+            picDisplay.Invalidate();
+        }
+
+        
+
+    }
+}
---
+++ Particle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace WindowsFormsApp1
+{
+    class Particle
+    {
+        public int Radius; // радуис частицы
+        public float X; // X координата положения частицы в пространстве
+        public float Y; // Y координата положения частицы в пространстве
+
+        public float Direction; // направление движения
+        public float Speed; // скорость перемещения
+        public float Life;
+
+        // добавили генератор случайных чисел
+        public static Random rand = new Random();
+
+        // конструктор по умолчанию будет создавать кастомную частицу
+        public Particle()
+        {
+            // я не трогаю координаты X, Y потому что хочу, чтобы все частицы возникали из одного места
+            Direction = rand.Next(360);
+            Speed = 1 + rand.Next(10);
+            Radius = 2 + rand.Next(10);
+            Life = 20 + rand.Next(100); // Добавили исходный запас здоровья от 20 до 120
+        }
+
+       
+
+
+        public void Draw(Graphics g)
+        {
+            // рассчитываем коэффициент прозрачности по шкале от 0 до 1.0
+            float k = Math.Min(1f, Life / 100);
+            // рассчитываем значение альфа канала в шкале от 0 до 255
+            // по аналогии с RGB, он используется для задания прозрачности
+            int alpha = (int)(k * 255);
+
+            // создаем цвет из уже существующего, но привязываем к нему еще и значение альфа канала
+            var color = Color.FromArgb(alpha, Color.Black);
+            var b = new SolidBrush(color);
+
+            // остальное все так же
+            g.FillEllipse(b, X - Radius, Y - Radius, Radius * 2, Radius * 2);
+
+            b.Dispose();
+        }
+    }
+}
